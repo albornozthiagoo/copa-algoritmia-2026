36356e2a2a32503089f6b7e0fd13787be410ecdb
valentin <vzaccari33@gmail.com>
2026-05-08 16:23:54 -0300

    Update desafio1_ranking_fifa.py

--- Desafio 1/codigo/desafio1_ranking_fifa.py
+++ Desafio 1/codigo/desafio1_ranking_fifa.py
@@ -58,12 +58,18 @@ def procesar_partido(equipos, local, visitante, goles_local, goles_visitante):
     equipos[visitante]["gf"] = equipos[visitante]["gf"] + goles_visitante
     equipos[visitante]["gc"] = equipos[visitante]["gc"] + goles_local
 
-    #
+    # preguntamos aca si los goles del equipo local es mayor a los goles del equipo visitante
+    # si se cumple sumamos +3 en puntos y +1 en partido ganado para el equipo local
+    # y como el equipo local gano sumamos +1 en el contador de partido perdido para el visitante
 
     if goles_local > goles_visitante:
         equipos[local]["puntos"] = equipos[local]["puntos"] + 3
         equipos[local]["pg"] = equipos[local]["pg"] + 1
         equipos[visitante]["pp"] = equipos[visitante]["pp"] + 1
+
+    # preguntamos aca si los goles del equipo local es menor a los goles del equipo visitante
+    # si se cumple sumamos +3 en puntos y +1 en partido ganado para el equipo visitante
+    # y como el equipo visitante gano sumamos +1 en el contador de partido perdido para el equipo local
 
     elif goles_local < goles_visitante:
         equipos[visitante]["puntos"] = equipos[visitante]["puntos"] + 3

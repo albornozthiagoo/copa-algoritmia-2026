5cbf771ca9c8e16877c026acff259014e688a9c8
valentin <vzaccari33@gmail.com>
2026-05-08 14:01:34 -0300

    Update desafio1_ranking_fifa.py

--- Desafio 1/codigo/desafio1_ranking_fifa.py
+++ Desafio 1/codigo/desafio1_ranking_fifa.py
@@ -1,6 +1,14 @@
 # con esta funcion creamos un equipo nuevo con todos sus estadisticas en cero
 # cada vez que se detecte o lee un equipo nuevo va seguir esta misma estructura
 
+#puntos = puntos del equipo
+#pj = partidos jugados
+#pg = partidos ganados
+#pe = partidos empatados
+#pp = partidos perdidos
+#gf = goles a favor
+#gc = goles en contra
+
 def crear_equipo():
     equipo = {
         "puntos": 0,

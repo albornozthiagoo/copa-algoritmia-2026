1f803ef484f21c7d65d6dd03df30506a67a602a2
Thiago Albornoz <tatotatuaje099@gmail.com>
2026-05-19 18:10:38 -0300

    subo desafio 3 e intento crear cancha

---
+++ challenges/desafio_3/codigo/desafio3.py
@@ -0,0 +1,43 @@
+"""
+BLOQUE 1
+CREACION DE LA CANCHA
+"""
+
+# funcion encargada de crear una cancha vacia
+def crear_cancha():
+
+    # creamos una lista vacia donde vamos a guardar toda la cancha
+    cancha = []
+
+    # recorremos el largo de la cancha
+    for largo in range(100):
+
+        # creamos una nueva franja de la cancha vacia
+        franja_cesped = []
+
+        # recorremos el ancho de la cancha
+        for ancho in range(60):
+
+            # agregamos una posicion vacia simulada con un "."
+            franja_cesped.append(".")
+
+        # cuando la franja esta completa la agregamos a la cancha
+        cancha.append(franja_cesped)
+
+    # devolvemos la cancha ya creada
+    return cancha
+
+"""
+BLOQUE 2
+IMPRIMIR LA CANCHA
+"""
+
+"""
+BLOQUE 3
+CREAR JUGADORES
+"""
+
+"""
+
+"""
+
